b0e1485cfe1541bdce71330e90353343bdc9347d
SDen4 <asw325@yandex.ru>
2021-09-20 21:22:52 +0500

    Updates

---
+++ static/js/main.3ee22e8a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpremote_calendar=this.webpackJsonpremote_calendar||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),a=t(3),o=t.n(a),s=(t(8),t(9),t(0));var i=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Remote calendar"})})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.3ee22e8a.chunk.js.map
