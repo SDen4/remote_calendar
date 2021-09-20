d55614fdf7a87720d7b42dcd75bf9ade5326d92e
SDen4 <asw325@yandex.ru>
2021-09-20 21:42:51 +0500

    Calendar component draft

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Calendar from '../Calendar';
 
 import styles from './Root.module.css';
 
 const Root: React.FC = () => {
-  return <div className={styles.root_wrapper}>Root</div>;
+  return (
+    <div className={styles.root_wrapper}>
+      <header>
+        <h1>Remote Calendar</h1>
+      </header>
+
+      <section className={styles.section_wrapper}>
+        <Calendar />
+      </section>
+    </div>
+  );
 };
 
 export default Root;
