d25436e7789426102147d102de24827f9c8d1b85
SDen4 <asw325@yandex.ru>
2021-10-03 12:44:55 +0500

    remoove Modal component

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
 
-import Modal from '../../components/Modal';
+import Modal from '../../ui/Modal';
 import Calendar from '../../components/Calendar';
 
 import { InitialStateType } from '../../store/types';
