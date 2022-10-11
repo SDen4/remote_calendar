600423388d78fe573e5c7af2b402be830a48be92
SDen4 <asw325@yandex.ru>
2022-10-11 20:09:20 +0500

    refactor: clear

--- src/constants/table.ts
+++ src/constants/table.ts
@@ -1,15 +1,8 @@
 import { IData } from '../store/types';
 
 export const initialData: IData[] = [
-  {
-    name: 'Total office',
-  },
-  {
-    name: 'Total remote',
-  },
+  { name: 'Total office' },
+  { name: 'Total remote' },
 ];
 
-export const defaultCell = {
-  row: '',
-  cell: '',
-};
+export const defaultCell = { row: '', cell: '' };
